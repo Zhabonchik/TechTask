6cf36580de369211469ab8fa8f9af6c7f628e972
Zhabonchik <shapelnik@mail.ru>
2024-11-20 19:42:06 +0300

    added skeleton for test classes

---
+++ src/test/java/com/ifortex/bookservice/service/impl/MemberServiceImplTest.java
@@ -0,0 +1,15 @@
+package com.ifortex.bookservice.service.impl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MemberServiceImplTest {
+    @Test
+    void findMember() {
+    }
+
+    @Test
+    void findMembers() {
+    }
+}
